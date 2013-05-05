bd7e8029184ebb3c24738c1a9cab3bec7db3f028
Danilo Antonio Garcia Sales <danilogarcia024@gmail.com>
2013-05-04 20:01:55 -0500

    Comment model validations added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :issue
   has_many :photos, as: :imageable, dependent: :delete_all
+
+  #Validations
+  validates :content, presence: true
 end
