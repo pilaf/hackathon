4fca1dc6c547c2b7bb29e35a93ad7a0330fda0a7
Danilo Antonio Garcia Sales <danilogarcia024@gmail.com>
2013-05-04 20:28:48 -0500

    Syntax error from photo model fixed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,5 @@ class Photo < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
 
   #Validations
-  validates :image, :imageable_type presence: true
+  validates :image, :imageable_type, presence: true
 end
